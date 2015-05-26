fd53e05f62e4b71ade5fd50e0099c82948aaf2b3
Olof-Joachim Frahm <olof@macrolet.net>
2015-05-26 13:50:15 +0100

    Add script to hide YouTube comments.

---
+++ disable-youtube-comments.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name        Youtube: Hide Comments
+// @namespace   https://github.com/Ferada/stylish
+// @description Hide the comments section entirely
+// @include     https://www.youtube.com/*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+document.getElementById ("watch-discussion").style.display = "none";
